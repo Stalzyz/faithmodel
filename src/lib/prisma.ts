28da7c1f37e5a90bed5fc1f0f450e26d4603cfff
Stalzyz <mstalinkumar05@gmail.com>
2026-07-12 03:19:29 +0530

    Remove hardcoded SQLite connection string

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-process.env.DATABASE_URL = "file:./dev.db";
-
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
