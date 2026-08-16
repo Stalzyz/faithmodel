197df70355928b4858d646536713a188d7502e5c
Stalzyz <mstalinkumar05@gmail.com>
2026-08-16 18:08:33 +0530

    Update run-rebuild.sh with password authentication option

--- run-rebuild.sh
+++ run-rebuild.sh
@@ -4,7 +4,7 @@ set timeout -1
 set password "Photoshop09@"
 set ip "200.97.163.236"
 
-spawn ssh -o StrictHostKeyChecking=no root@$ip "cd /var/www/faithmodel && npm run build && pm2 restart faithmodel"
+spawn ssh -o StrictHostKeyChecking=no -o PubkeyAuthentication=no root@$ip "cd /var/www/faithmodel && npm run build && pm2 restart faithmodel"
 expect {
     "password:" {
         send "$password\r"
