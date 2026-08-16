66561ccb763acef359ab6594b11c569ee78d0a93
Stalzyz <mstalinkumar05@gmail.com>
2026-08-16 16:15:50 +0530

    Force password authentication in run-certbot.sh

--- run-certbot.sh
+++ run-certbot.sh
@@ -4,7 +4,7 @@ set timeout -1
 set password "Photoshop09@"
 set ip "200.97.163.236"
 
-spawn ssh -o StrictHostKeyChecking=no root@$ip "sed -i 's/server_name fm.grekam.in fmadmin.grekam.in;/server_name fm.grekam.in;/g' /etc/nginx/sites-available/faithmodel && systemctl reload nginx && certbot --nginx -d fm.grekam.in --non-interactive --agree-tos -m stalin@grekam.in"
+spawn ssh -o StrictHostKeyChecking=no -o PubkeyAuthentication=no root@$ip "sed -i 's/server_name fm.grekam.in fmadmin.grekam.in;/server_name fm.grekam.in;/g' /etc/nginx/sites-available/faithmodel && systemctl reload nginx && certbot --nginx -d fm.grekam.in --non-interactive --agree-tos -m stalin@grekam.in"
 expect {
     "password:" {
         send "$password\r"
